3949fb0dd218b5727d89520bb16ee49b65c048ec
Pavan Yadiki <mail2pavanyr@gmail.com>
2015-03-01 01:22:27 -0600

    Added the source files

---
+++ SynchronousGHS/src/FloodMax.java
@@ -0,0 +1,13 @@
+/* Group Members-
+ Pavan Kumar Yadiki Rajakumar- 202 121 7437
+ Sameer Nadgouda- 202 121 3812
+ Tejasvee Bolisetty- 202 122 5895*/
+
+public class FloodMax {
+	public static void main(String[] args){
+		Master m=new Master();
+		m.setMasterObject(m);
+		m.getInput();
+		m.masterThread("master");
+	}
+}
